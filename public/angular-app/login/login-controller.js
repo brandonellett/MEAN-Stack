99767b95ff387cfe301ef21d15195544ab6b283f
Brandon Ellett <brandonellett@gmail.com>
2016-10-28 11:53:03 -0600

    Completed MEAN Hotel App

--- public/angular-app/login/login-controller.js
+++ public/angular-app/login/login-controller.js
@@ -1,6 +1,6 @@
 angular.module('meanhotel').controller('LoginController', LoginController);
 
-function LoginController($http, $location, $window, AuthFactory){
+function LoginController($http, $location, $window, AuthFactory, jwtHelper){
     var vm = this;
     
     vm.isLoggedIn = function(){
@@ -13,21 +13,26 @@ function LoginController($http, $location, $window, AuthFactory){
     vm.login = function(){
         if(vm.username && vm.password){
             var user = {
-                username: username,
-                password: password
+                username: vm.username,
+                password: vm.password
             };
         $http.post('/api/users/login', user).then(function(response){
             if(response.data.success){
                 $window.sessionStorage.token = response.data.token;
                 AuthFactory.isLoggedIn = true;
+                var token = $window.sessionStorage.token;
+                var decodedToken = jwtHelper.decodeToken(token);
+                vm.loggedInUser = decodedToken.username;
             }
         }).catch(function(error){
             console.log(error);
-        })    
+        });    
         }    
     }
     vm.logout = function(){
-        
+        AuthFactory.isLoggedIn = false;
+        delete $window.sessionStorage.token;
+        $location.path('/');
     }
     vm.isActiveTab = function(url){
         var currentPath = $location.path().split('/')[1];
